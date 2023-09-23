d04c6b5fb4e7f4636489f942b69a08885100de76
Andrey Alexandre <andreyalexandre16@gmail.com>
2023-09-23 19:50:30 -0300

    Criado modelo para receber os dados dos jogos

--- web/soccer/matches/models.py
+++ web/soccer/matches/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Matches(models.Model):
+    Data = models.DateTimeField()
+    GameId = models.IntegerField(primary_key=True)
+    Time = models.TextField()
+    Local = models.TextField()
+    TipoMetrica = models.TextField()
+    JogosCasa = models.IntegerField()
+    JogosFora = models.IntegerField()
+    Goal_T1 = models.FloatField()
+    Goal_T2 = models.FloatField()
+    GoalOver_T1 = models.IntegerField()
+    GoalOver_T2 = models.IntegerField()
+    Corner_T1 = models.FloatField()
+    Corner_T2 = models.FloatField()
+    CornerOver_T1 = models.IntegerField()
+    CornerOver_T2 = models.IntegerField()
+    Win_T1 = models.TextField()
+    Win_T2 = models.TextField()
+    url = models.TextField()
